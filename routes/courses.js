f177ad97ce10096b5dea6193bf6405846e72b0ef
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2024-01-04 21:15:37 +0200

    Advanced Results Middleware

--- routes/courses.js
+++ routes/courses.js
@@ -7,9 +7,18 @@ const {
   deleteCourse,
 } = require('../controllers/courses');
 
+const Course = require('../models/Course');
+const advancedResults = require('../middleware/advancedResults');
+
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getCourses);
+router.route('/').get(
+  advancedResults(Course, {
+    path: 'bootcamp',
+    select: 'name description',
+  }),
+  getCourses
+);
 router.route('/').post(addCourse);
 router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
